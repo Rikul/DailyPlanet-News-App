9b95ae021905d566037a60033a6478776d780940
Rikul patel <rikulpatel@gmail.com>
2025-12-25 22:21:32 -0600

    newsapi free key

--- app/src/main/java/com/example/dailyplanet/api/Constants.kt
+++ app/src/main/java/com/example/dailyplanet/api/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.dailyplanet.api
 
 object Constants {
-    //const val API_KEY = "f1d33c3f03334b3ba896b7fcd43f6f7e" // IMPORTANT: Get your key from newsapi.org
-    const val API_KEY = "eba4fb39458d417390751a7014e5a073"  // CHANGE THIS
+    const val API_KEY = "getafreeapikeyfrom_newsapi.org"  // CHANGE THIS
     const val BASE_URL = "https://newsapi.org"
 }
